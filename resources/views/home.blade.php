4e34d982843b218ae06833622a1ee3571952e6b6
Yancx-dev <ybabuskin@gmail.com>
2024-12-05 22:24:31 +0300

    Добавил layout/app, bootstrap

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+
+    @section('content')
+        <main class="flex-grow-1 min-vh-100">
+        <div class="container">
+            Тест1
+        </div>
+        </main>
+    @endsection
+
+
