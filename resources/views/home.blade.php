f038232c8c614ba55bf6aa76453a4d83bc3308f9
Yancx-dev <ybabuskin@gmail.com>
2024-12-08 15:31:36 +0300

    Тест компонентов livewire

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,8 @@
     @section('content')
         <main class="flex-grow-1 min-vh-100">
             <div class="container">
-
+                @livewire('counter')
+                @livewire('counter')
             </div>
         </main>
     @endsection
